7d7b837af8e0d163c6415a2a582b403586d515e9
RobinLodrigo <robin.lodrigo.u8341@student.hogent.be>
2015-11-19 10:47:49 +0100

    Authentication endpoints

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
+
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 require('./models/Posts');
 require('./models/Comments');
 require('./models/Users');
